c2d2a75694961f71aebb4abe59bf76a492dfac51
mahmoudibrahimmohammad <mahmoud.ibrahim.mohammad@rikstv.no>
2022-12-08 19:34:25 +0100

    implemented simple sign out functionality in MainScreen.kt

--- app/src/main/java/com/example/complete_firebase/screens/MainScreen.kt
+++ app/src/main/java/com/example/complete_firebase/screens/MainScreen.kt
@@ -8,14 +8,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.example.complete_firebase.components.PrimaryButton
+import com.example.complete_firebase.navigation.Screen
+import com.google.firebase.auth.FirebaseAuth
 
 @Composable
-fun MainScreen(navController: NavController) {
+fun MainScreen(navController: NavController, auth: FirebaseAuth) {
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxSize()
     ) {
         Text(text = "Main Screen")
+        PrimaryButton(
+            text = "Sign Out",
+            onClick = {
+                auth.signOut()
+                navController.navigate(Screen.LoginScreen.getArg())
+            }
+        )
     }
 }
