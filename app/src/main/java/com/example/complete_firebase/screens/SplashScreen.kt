9d19e04017619fb2c806d34f3f1d567056c9aded
mahmoudibrahimmohammad <mahmoud.ibrahim.mohammad@rikstv.no>
2022-12-08 19:35:14 +0100

    skip login screen if user already authenticated

--- app/src/main/java/com/example/complete_firebase/screens/SplashScreen.kt
+++ app/src/main/java/com/example/complete_firebase/screens/SplashScreen.kt
@@ -1,14 +1,12 @@
 package com.example.complete_firebase.screens
 
+import android.util.Log
 import android.view.animation.OvershootInterpolator
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -20,10 +18,11 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.complete_firebase.R
 import com.example.complete_firebase.navigation.Screen
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen(navController: NavController) {
+fun SplashScreen(navController: NavController, auth: FirebaseAuth) {
     val scale = remember {
         androidx.compose.animation.core.Animatable(0f)
     }
@@ -39,7 +38,10 @@ fun SplashScreen(navController: NavController) {
                 })
         )
         delay(1000L)
-        navController.navigate(Screen.LoginScreen.getArg())
+        if (auth.currentUser != null) {
+            navController.navigate(Screen.MainScreen.getArg())
+            Log.d(TAG, "SplashScreen: ${auth.currentUser?.email}")
+        } else navController.navigate(Screen.LoginScreen.getArg())
     }
 
     // Image
@@ -50,7 +52,8 @@ fun SplashScreen(navController: NavController) {
             painter = painterResource(id = R.drawable.firebase),
             contentDescription = "Firebase App",
             modifier = Modifier
-                .fillMaxSize().scale(scale.value)
+                .fillMaxSize()
+                .scale(scale.value)
         )
     }
 }
